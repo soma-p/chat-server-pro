3f10b8c02d0875584d6ff05357f9cadec1586d78
Pranav Soma <prsoma@ieng6-201.ucsd.edu>
2024-02-29 15:21:08 -0800

    Fixed all bugs

--- ChatServer.java
+++ ChatServer.java
@@ -84,13 +84,12 @@ class ChatHandler implements URLHandler {
                 numberOfExclamationMarks += 1;
               }
               if (new String(Character.toChars(character)).equals("😂")) {
-                analysis = " This message has a LOL vibe.";
+                analysis += " This message has a LOL vibe.";
               }
               if (new String(Character.toChars(character)).equals("🥹")) {
-                analysis = " This message has a awwww vibe.";
-              } else {
+                analysis += " This message has a awwww vibe.";
+              }
               characterIndex += 1;
-               }
             }
             if (numberOfExclamationMarks > 2) {
               analysis += " This message ends forcefully.";
